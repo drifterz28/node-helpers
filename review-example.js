07df435dc2298428e08054ca1267133443ce388c
Whitney, Christopher <CWhitney@webmd.net>
2015-04-30 14:59:54 -0700

    add exampkle review

---
+++ review-example.js
@@ -0,0 +1,37 @@
+POWERREVIEWS.common.gResult['content/10/03/mafipla1sunp-en_US-1-reviews.js'] = [{
+    r: {
+        id: 53369383,
+        v: 0,
+        t: 0,
+        r: 4, // rating
+        h: "Fantastic filament!", // title
+        n: "Gatekeeper Calvin", // name
+        l: "en_US",
+        w: "Pittsburgh, PA", // location
+        b: {
+            n: "Bottom Line",
+            k: "Yes"
+        },
+        o: "w",
+        d: "12\/29\/2014", //date added
+        db: "2014-12-29T22:20:17", // V message V
+        p: "Bought this to make Christmas gifts for my friends and family. I\'d had some... forgettable experiences with filament from Makergeeks in the past (though some quite memorably good ones as well!), and decided to give the new &quot;Maker filament&quot; a chance. Figured with the price, I didn\'t have much to loose. I can confidently say, I didn\'t loose a single thing!<br \/><br \/>Don\'t question the quality of the filament from the price, it\'s as good as any $50 roll I\'ve ever used! It prints like a dream, it flows through the hot end like oiled butter, and sticks to the bed without hassle. The parts I\'ve made with it are structurally sound, both with the novelty items I\'ve made (tons and tons of pikachu figures) and the structural components I\'ve made (fan shroud for an overheating motor on my printer) have held strong and effective.<br \/><br \/>The only disappointment I had with this filament was that from the picture, the plastic looked opaque. In person, the filament I got was quite obviously transparent. On prints with several perimeters and even moderate infill, prints will not be transparent, but on single-walled vases light will escape through.<br \/><br \/>Save for the lack of opacity, I can confidently say that this is some of the best-valued filament available for purchase! Would and WILL buy again!",
+        g: [{ // not used below.
+            n: "Pros",
+            k: "pros",
+            v: ["Performance", "price", "quality"]
+        }, {
+            n: "Cons",
+            k: "cons",
+            v: ["Transparency", "Unexpectedly Transparent"]
+        }, {
+            n: "Best Uses",
+            k: "bestuses",
+            v: ["Gifts", "Parts"]
+        }, {
+            n: "Was this a gift?",
+            k: "wasthisagift",
+            v: ["No"]
+        }]
+    }
+}];
